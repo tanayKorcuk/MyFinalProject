f28d61efc4bf5bfa01d3d533aaccfe1a64cacdd2
T.K <tankorcuk@gmail.com>
2023-01-23 18:02:43 +0300

    day11 begining

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,31 @@
+﻿using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]//attribute/annotation:javada
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public List<Product> GetAllProducts()
+        {
+
+            return new List<Product>
+            {
+                new Product { ProductId = 1, ProductName = "Elma" },
+                new Product { ProductId = 2, ProductName = "Armut" },
+            };
+            
+         }
+    
+    
+    
+    
+    }
+}
